96aa1fc2d782e938da2cd3c0b2c51d91fd726936
yuanye <yey000@sjtu.edu.cn>
2018-08-14 01:31:45 +0800

    导入其他包

---
+++ cac.py
@@ -0,0 +1,5 @@
+Pi = 3.14;
+
+def calc_area(radiis):
+    print "we are in %s" %__name__;
+    print Pi * (radiis ** 2);
--- showtime.py
+++ showtime.py
@@ -1,9 +1,12 @@
 # -*- coding: UTF-8 -*-
+import cac
 # print "ok";
 # print "haha";
 
 def main():
+    cac.calc_area(2);
     print "we are in %s" %__name__
 
 if __name__ == '__main__':
-    main();
+    main()
+
